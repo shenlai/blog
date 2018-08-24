d8aeb798079c475f9af95b163db90dd99da0bf95
shenlai <shlai244@163.com>
2018-08-24 19:37:33 +0800

    使用maven代替gradle重新构建项目

---
+++ src/main/resources/static/js/admins/main.js
@@ -0,0 +1,28 @@
+
+// DOM 加载完再执行
+$(function() {
+
+	// 搜索
+	$(".blog-menu .list-group-item").click(function() {
+		console($(this).value);
+		//移除其他点击样式，添加当前的点击样式
+		$(".blog-menu .list-group-item").removeClass("active");
+		$(this).addClass("active");
+		
+		var url = $(this).attr("url");
+		//加载表格内容
+		$.ajax({
+			url:url,
+			success:function(data){
+				$("#rightContainer").html(data);
+			},
+			error:function(){
+				alert("error");
+			}
+		});
+		
+	});
+	
+	//选中菜单第一页
+	$(".blog-menu .list-group-item:first").trigger("click");
+});
